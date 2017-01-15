6643df358a8f5c087d133c3fbfa5c9ae1133cefa
siqister <siqi.zhu@gmail.com>
2017-01-14 21:29:36 -0500

    First commit

---
+++ public/script.js
@@ -0,0 +1,63 @@
+d3.queue()
+	.defer(d3.csv,'./data/hubway_trips_reduced.csv',parseTrips)
+	.defer(d3.csv,'./data/hubway_stations.csv',parseStations)
+	.await(dataLoaded);
+
+function dataLoaded(err,trips,stations){
+	console.log(trips);
+	console.log(stations);
+
+	//create a crossfilter
+	var cf = crossfilter(trips);
+
+	//Create dimensions on duration, startStn, endStn, startTime, endTime, userType
+	var tripsByDuration = cf.dimension(function(d){return d.duration});
+	//...
+
+/*	Part 1: the Basics
+	Crossfilter dimensions have the .top() and .bottom() methods to return records from the crossfilter
+*/	
+	//Top 50 trips by duration?
+
+	//All trips, sorted by startTime?
+
+}
+
+function parseTrips(d){
+	return {
+		bike_nr:d.bike_nr,
+		duration:+d.duration,
+		startStn:d.strt_statn,
+		startTime:parseTime(d.start_date),
+		endStn:d.end_statn,
+		endTime:parseTime(d.end_date),
+		userType:d.subsc_type,
+		userGender:d.gender?d.gender:undefined,
+		userBirthdate:d.birth_date?+d.birth_date:undefined
+	}
+}
+
+function parseStations(d){
+	return {
+		id:d.id,
+		lngLat:[+d.lng,+d.lat],
+		city:d.municipal,
+		name:d.station,
+		status:d.status,
+		terminal:d.terminal
+	}
+}
+
+function parseTime(timeStr){
+	var time = timeStr.split(' ')[1].split(':'),
+		hour = +time[0],
+		min = +time[1],
+		sec = +time[2];
+
+	var	date = timeStr.split(' ')[0].split('/'),
+		year = date[2],
+		month = date[0],
+		day = date[1];
+
+	return new Date(year,month-1,day,hour,min,sec);
+}
